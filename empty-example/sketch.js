8c41cd4d664efbdd1f5d6086422ccacf872cb592
Fanny <fanny.f.nilsson@hyperisland.se>
2020-05-15 11:28:48 +0200

    setting up p5

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,26 @@
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
+  background(185, 75, 53);
+
+  //main circle
+  stroke(219, 209, 203);
+  strokeWeight(40);
+  fill(28);
+  circle(200, 200, 200);
+
+  //blue circle
+  noStroke();
+  fill(82, 133, 184);
+  arc(200, 200, 241, 241, radians(270), radians(450));
+
+  //yellow circle
+  fill(219, 175, 70);
+  arc(200, 200, 159, 159, radians(270), radians(450));
+
+  //pink circle
+  fill(212, 133, 121);
+  arc(200, 200, 80, 80, radians(270), radians(450));
 }
