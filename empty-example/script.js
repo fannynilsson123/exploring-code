e9a677ae6a9b260b092901eecfda7641c724d312
Fanny <fanny.f.nilsson@hyperisland.se>
2020-05-20 12:57:01 +0200

    lagt till map funktion

--- empty-example/script.js
+++ empty-example/script.js
@@ -1,11 +1,10 @@
+
 fetch('https://api.covid19api.com/summary')
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        let array = [];
-        array.push(data)
+        document.getElementById('countries').innerHTML = data.Countries.map(country => { return country.Country });
 
-
-        console.log(array);
+        console.log(data);
     });
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,27 +1,17 @@
 
+var advice;
 function setup() {
   createCanvas(400, 400);
+  loadJSON('https://api.covid19api.com/summary', gotData)
 }
 
+function gotData(data) {
+  console.log(data);
+  advice = data;
+}
 function draw() {
-  background(185, 75, 53);
-
-  //main circle
-  stroke(219, 209, 203);
-  strokeWeight(40);
-  fill(28);
-  circle(200, 200, 200);
-
-  //blue circle
-  noStroke();
-  fill(82, 133, 184);
-  arc(200, 200, 241, 241, radians(270), radians(450));
-
-  //yellow circle
-  fill(219, 175, 70);
-  arc(200, 200, 159, 159, radians(270), radians(450));
-
-  //pink circle
-  fill(212, 133, 121);
-  arc(200, 200, 80, 80, radians(270), radians(450));
+  background(0);
+  if (advice) {
+    circle(50, advice.Global.NewConfirmed, advice.Global.NewConfirmed);
+  }
 }
