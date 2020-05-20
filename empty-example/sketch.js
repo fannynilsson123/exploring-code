f4a0b6c6af7c7e89418387dd9a223ee5b8faa8f3
Fanny <fanny.f.nilsson@hyperisland.se>
2020-05-20 15:26:54 +0200

    printat data för globala siffror i p5

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,17 +1,26 @@
 
-var advice;
+var global;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   loadJSON('https://api.covid19api.com/summary', gotData)
 }
 
 function gotData(data) {
   console.log(data);
-  advice = data;
+  global = data;
 }
 function draw() {
-  background(0);
-  if (advice) {
-    circle(50, advice.Global.NewConfirmed, advice.Global.NewConfirmed);
+  background(225);
+  if (global) {
+    textSize(100)
+    text(global.Global.NewConfirmed, 100, 200)
+    text(global.Global.NewDeaths, 900, 220)
+    text(global.Global.NewRecovered, 250, 400)
+    text(global.Global.TotalConfirmed, 660, 650)
+    text(global.Global.TotalDeaths, 300, 600)
+    text(global.Global.TotalRecovered, 600, 500)
+
+    // var countries = global.Countries.map(country => { return country.Country });
+    // text(countries, 10, 100)
   }
 }
