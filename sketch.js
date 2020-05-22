ce8343c634738c8217ba6086556e0407858bb1bf
Fanny <fanny.f.nilsson@hyperisland.se>
2020-05-22 13:39:01 +0200

    changed font

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 
 var global;
+
+var index = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   loadJSON('https://api.covid19api.com/summary', gotData)
@@ -10,27 +13,31 @@ function gotData(data) {
   global = data;
 }
 function draw() {
-  background(225);
+  background(0);
+
   if (global) {
+
+    textFont('Inter')
     textSize(90)
     text(global.Global.NewConfirmed, 200, 150)
-    text(global.Global.NewDeaths, 900, 220)
+    text(global.Global.NewDeaths, 1000, 220)
     text(global.Global.NewRecovered, 550, 350)
-    text(global.Global.TotalConfirmed, 120, 500)
+    text(global.Global.TotalConfirmed, 100, 500)
     text(global.Global.TotalDeaths, 950, 480)
     text(global.Global.TotalRecovered, 550, 620)
 
     textSize(25)
     text("New Confirmed", 200, 70)
-    text("New Deaths", 900, 140)
+    text("New Deaths", 1000, 140)
     text("New Recoverd", 550, 270)
-    text("Total Confirmed", 120, 420)
+    text("Total Confirmed", 100, 420)
     text("Total Deaths", 950, 400)
     text("Total Recoverd", 550, 540)
+    fill(255)
 
     // var countries = global.Countries.map(country => { return country.Country });
     // text(countries, 10, 100)
 
-    // text(global.Global.TotalConfirmed, 700(vågrätt), 220(lodrätt))
+    // x(vågrätt), y(lodrätt))
   }
 }
